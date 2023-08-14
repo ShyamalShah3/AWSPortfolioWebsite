c76508e520d840996d4d906bddfedb850d55d08d
ShyamalShah3 <shyamalgs@gmail.com>
2023-08-13 23:39:17 -0700

    Animated Cursor

--- portfolio-website/src/App.js
+++ portfolio-website/src/App.js
@@ -1,12 +1,15 @@
+import Layout from './Layout/Layout';
 import { NavBar } from './components/Navigation/NavBar';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <NavBar />
-    </div>
+    <Layout>
+      <div className="App">
+        <NavBar />
+      </div>
+    </Layout>
   );
 }
 
